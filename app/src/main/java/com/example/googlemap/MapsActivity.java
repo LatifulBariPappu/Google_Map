2f728133d49c0bb5007842ca51d91453428c0ccb
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-08-28 23:45:16 +0600

    map activity

--- app/src/main/java/com/example/googlemap/MapsActivity.java
+++ app/src/main/java/com/example/googlemap/MapsActivity.java
@@ -1,14 +1,31 @@
 package com.example.googlemap;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
 import android.os.Bundle;
-
-public class MapsActivity extends AppCompatActivity {
-
+import android.widget.FrameLayout;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    GoogleMap gMap;
+    FrameLayout map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        map=findViewById(R.id.map);
+        SupportMapFragment mapFragment=(SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.gMap=googleMap;
+        LatLng mapBangladesh=new LatLng(23.87275,90.30955);
+        this.gMap.addMarker(new MarkerOptions().position(mapBangladesh).title("Marker in Bangladesh"));
+        this.gMap.moveCamera(CameraUpdateFactory.newLatLng(mapBangladesh));
     }
 }
