a425acb79820f6781e898a00997eaaac85b6d55b
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-09-12 19:19:13 +0600

    get current location

--- app/src/main/java/com/example/googlemap/MapsActivity.java
+++ app/src/main/java/com/example/googlemap/MapsActivity.java
@@ -1,25 +1,44 @@
 package com.example.googlemap;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.widget.FrameLayout;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-    GoogleMap gMap;
+    Location currentLocation;
+    FusedLocationProviderClient fusedClient;
+    private static final int REQUEST_CODE=101;
     FrameLayout map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         map=findViewById(R.id.map);
-        SupportMapFragment mapFragment=(SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        fusedClient= LocationServices.getFusedLocationProviderClient(this);
+        getLocation();
     }
+    private void getLocation(){
+        if(ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
+        && ActivityCompat.checkSelfPermission(
+                this,Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
+                return;
+            }
+        }
+
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
